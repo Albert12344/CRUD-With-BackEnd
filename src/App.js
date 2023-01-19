6d8829592dd9a41a7d415b74d9265910cd41a9c8
Albert <albert.manukyan.005@gmail.com>
2023-01-19 12:34:30 +0400

    commit, commit, commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import GetEditDelete from './frontEnd/GetEditDelete';
+import Css from "./frontEnd/Css";
 
 function App() {
   return (
       <div className="App">
           <GetEditDelete/>
+          <Css/>
       </div>
   );
 }
--- src/frontEnd/GetEditDelete.js
+++ src/frontEnd/GetEditDelete.js
@@ -1,9 +1,8 @@
 import React, {useEffect, useState,useRef} from 'react'
 import axios from 'axios';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+
+
 
 export default function Add() {
     const [posts, setPosts] = useState([])
@@ -11,104 +10,86 @@ export default function Add() {
     const [value1, setValue1] = useState("");
     const inputReset = useRef(null);
     const inputReset1 = useRef(null)
-    const [isShown, setIsShown] = useState(false)
-    const [count, setCount] = useState(0)
-    const [isShow, invokeModal] = React.useState(false)
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const [isShown, setIsShown] = useState(false);
+    const [value3, setValue3] = useState("");
+    const [value4, setValue4] = useState("");
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 600,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 15,
-      };
-    const submitValue = () => {
-        const person = {
+
+const submitValue = () => {
+    axios.post('http://localhost:4000/crud', {
+        firstName: value,
+        lastName: value1,
+    })
+}
+
+const onDelete = (_id) => {
+    axios.delete(`http://localhost:4000/crud/${_id}`)
+}
+
+const onUpdate = (_id) => {
+    if(posts._id !== posts._id) {
+        axios.put(`http://localhost:4000/crud/${_id}`, {
             firstName: value,
-            lastName: value1,
-        };
-        axios.post('http://localhost:4000/crud', person)
-    }
-
-    const onDelete = (_id) => {
-        axios.delete(`http://localhost:4000/crud/${_id}`)
-    }
-
-    const onUpdate = (_id) => {
-        const person = {
-            firstName: value,
-            lastName: value1,
-        };
-        axios.put(`http://localhost:4000/crud/${_id}`, person)
-        setIsShown(current => !current);
-        setOpen(true)
-    }
-
-    useEffect(() => {
-        axios.get('http://localhost:4000/crud')
-        .then(res => {
-        setPosts(res.data)
+            lastName: value1
         })
-    }, [onDelete]);
+    }
+    setIsShown(current => !current);
+}
 
-  return (
-    <div>
-        <div>
-            <div>
+const onUpdate1 = (_id) => {
+    if(value, value1 !== value3, value4) {
+        axios.put(`http://localhost:4000/crud/${_id}`, {
+            firstName: value3,
+            lastName: value4
+        })
+    }
+    setIsShown(current => !current);
+}
+
+
+useEffect(() => {
+    axios.get('http://localhost:4000/crud')
+    .then(res => {
+    setPosts(res.data)
+    })
+}, [onDelete, onUpdate1]);
+
+return (
+        <div className='container'>
+            
                 <form action="">
-                    <input type="text" placeholder='firstName' ref={inputReset} onChange={(e) => setValue(e.target.value)} />
-                    <input type="text" placeholder='lastName' ref={inputReset1} onChange={(e) => setValue1(e.target.value)} />
-                    <button onClick={submitValue}>Create</button>
+                <div className='postDiv'>
+                    <label>FirstName</label> 
+                    <TextField sx={{ m: 0.8 }} id="standard-basic"  variant="standard" ref={inputReset} onChange={(e) => setValue(e.target.value)}/>
+                    <label>LastName</label> 
+                    <TextField sx={{ m: 0.8 }} id="standard-basic" variant="standard" ref={inputReset1} onChange={(e) => setValue1(e.target.value)}/>
+                    <button className='create' onClick={submitValue}>Create</button>
+                    </div>
                 </form>
-            </div>
+            
+            <div>
+                {posts.map(post => { 
+                     return( 
+                <div key={post._id} className='dataDiv'>
+                    <h2 className='person'>{post.firstName + " " + post.lastName}</h2>
+                    <button className='delete' onClick={() => onDelete(post._id)}>Delete</button>
+                    {isShown && (
+                        <div className='editDiv'>
+                           <form action="">
+                               <label>FirstName</label> 
+                               <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard"  onChange={(e) => setValue3(e.target.value)}/> 
+                               <label>LastName</label> 
+                               <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard" onChange={(e) => setValue4(e.target.value)}/> 
+                               <button className='create' onClick={() => onUpdate1(post._id)}>Update</button> 
+                           </form> 
+                        </div>)}
+                </div>)})}
+                <div className='Edit'>
+                    {!!posts.length && <button  className='create' onClick={() => onUpdate(posts._id)}>Edit</button>}
+                </div>
+            </div>    
         </div>
-        <div>
-            {posts.map(post => { 
-                 return( 
-                    <div key={post._id}>
-                        <h2>{post.firstName + " " + post.lastName}</h2>
-                        <div>
-                            <button onClick={() => onDelete(post._id)}>Delete</button>
-                            <button onClick={() => onUpdate(post._id)}>Edit</button>
-                        </div>
-                        <Modal open={open} onClose={handleClose}>
-                        <Box sx={style}>
-                            <div className='modal'>
-                                <form action="">
-                                <label>FirstName</label>
-                                <input type="text" onChange={(e) => setValue(e.target.value)}/>
-                                <label>LastName</label>
-                                <input type="text" onChange={(e) => setValue1(e.target.value)}/>
-                                <button onClick={() => onUpdate(post._id)}>Update</button>
-                                </form>
-                            </div>
-                        </Box>
-                        </Modal>
-                    </div> 
-                    )
-                })
-            }
-        </div>
-    </div>
-        )
+    )
 };
 
 
-// {isShown && (
-    // <>
-    {/* <div className='modal'> */}
-        {/* <label>FirstName</label> */}
-        {/* <input type="text" onChange={(e) => setValue(e.target.value)}/> */}
-        {/* <label>LastName</label> */}
-        {/* <input type="text" onChange={(e) => setValue1(e.target.value)}/> */}
-        {/* <button onClick={() => onUpdate(post._id)}>Update</button> */}
-    {/* </div> */}
-    {/* </> */}
-    // )}
