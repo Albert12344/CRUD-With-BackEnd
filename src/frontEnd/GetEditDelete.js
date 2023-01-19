51a55510786d7d1ff7b96bb632f656c2dd4a22a6
Albert <albert.manukyan.005@gmail.com>
2023-01-19 13:06:41 +0400

    commit, commit, commit

--- src/frontEnd/GetEditDelete.js
+++ src/frontEnd/GetEditDelete.js
@@ -56,7 +56,7 @@ useEffect(() => {
 
 return (
         <div className='container'>
-            
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
                 <form action="">
                 <div className='postDiv'>
                     <label>FirstName</label> 
