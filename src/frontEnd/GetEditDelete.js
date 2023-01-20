7ce024193f11c553d4f6aae943d59e434d7bef8f
Albert <albert.manukyan.005@gmail.com>
2023-01-20 13:53:13 +0400

    I dont know

--- src/frontEnd/GetEditDelete.js
+++ src/frontEnd/GetEditDelete.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState,useRef} from 'react'
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
 
 export default function Add() {
     const [posts, setPosts] = useState([])
@@ -55,10 +54,8 @@ useEffect(() => {
 
 return (
         <div className='container'>
-            <div className='navbar'>
-            </div>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-                <form action="">
+            <div className='navbar'/>
+            <form action="">
                 <div className='postDiv'>
                     <label>FirstName</label> 
                     <TextField sx={{ m: 0.8 }} id="standard-basic"  variant="standard" ref={inputReset} onChange={(e) => setValue(e.target.value)}/>
@@ -67,32 +64,33 @@ return (
                     <button className='create' onClick={submitValue}>Create</button>
                     </div>
                 </form>
-            
-            <div>
                 {posts.map(post => { 
                      return( 
                 <div key={post._id} className='dataDiv'>
-                    <h2 className='person'>{post.firstName + " " + post.lastName}</h2>
-                    <button className='delete' onClick={() => onDelete(post._id)}>Delete</button>
+                    <div className='childdataDiv'>
+                        <h2 className='person'>{post.firstName + " " + post.lastName}</h2>
+                        <button className='delete' onClick={() => onDelete(post._id)}>Delete</button>
+                    </div>
                     {isShown && (
                         <div className='editDiv'>
-                           <form action="">
-                               <label>FirstName</label> 
-                               <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard"  onChange={(e) => setValue3(e.target.value)}/> 
-                               <label>LastName</label> 
-                               <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard" onChange={(e) => setValue4(e.target.value)}/> 
-                               <button className='create' onClick={() => onUpdate1(post._id)}>Update</button> 
-                           </form> 
+                            <div className='childeditDiv'>
+                                <form action="">
+                                   <label>FirstName</label> 
+                                   <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard"  onChange={(e) => setValue3(e.target.value)}/> 
+                                   <label>LastName</label> 
+                                   <TextField sx={{ m: 0.8 }} type="text" id="standard-basic"  variant="standard" onChange={(e) => setValue4(e.target.value)}/> 
+                                   <button className='create' onClick={() => onUpdate1(post._id)}>Update</button> 
+                                </form>
+                            </div>
                         </div>)}
                 </div>)})}
-                <div className='Edit'>
-                    {!!posts.length && <button  className='edit' onClick={() => onUpdate(posts._id)}>Edit</button>}
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-                    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet"></link>
-                </div>
-            </div>    
-        </div>
+            <div className='EditDiv1'>
+                {!!posts.length && <button  className='edit' onClick={() => onUpdate(posts._id)}>Edit</button>}
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
+                <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet"></link>
+            </div>     
+        </div>    
     )
 };
 
